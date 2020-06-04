6f97e39d7f609148d23c744fa8349aed8d6bd496
Nicholas Kaaz Omahen <nomahen@ucsc.edu>
2020-06-03 23:26:51 -0500

    Added structure of the code for Lx/Ly/Lz to be included, including interpolation from Q tables.

--- evolve_fvm.pyx
+++ evolve_fvm.pyx
@@ -165,7 +165,7 @@ def evolve(*p):
         _Lz = (1.0/(0.2*np.sqrt(2.0*np.pi))) * (np.exp(-0.5*((_x - 2.5)**2./0.2)) + 1.0)
         bc = "infinite"  
  
- 
+    ## currently, the following doesnt do anything
     # make bc_type
     if (bc == 'sink'):
         bc_type = 0
@@ -178,6 +178,23 @@ def evolve(*p):
     else:
         print "Error! bc needs to be set to \"sink\", \"outflow\", \"mix\" or \"infinite\"! Exiting"
         exit()
+
+
+    ########
+    ## Lets read in our Q coefficients
+    Q1_parsed = load_Q(Q1_path)
+    Q2_parsed = load_Q(Q2_path)
+    Q3_parsed = load_Q(Q3_path)
+    cdef int ng_Q = len(Q1_parsed) # Get length of Q
+    ## Psi array that Q1/Q2/Q3 correspond to
+    _s_arr = np.logspace(0,np.log10(smax+1),ng_Q) - 1
+    ## Now make cython arrays for each Q coefficient and the corresponding psi array so we can interpolate from it in the code. 
+    cdef double[:] Q1_arr = np.log10(-Q1_parsed + small)
+    cdef double[:] Q2_arr = np.log10(Q2_parsed + small)
+    cdef double[:] Q3_arr = np.log10(Q3_parsed + small)
+    cdef double[:] s_arr = _s_arr
+    ########
+
   
     ########
  
@@ -205,8 +222,11 @@ def evolve(*p):
     cdef double[:] Ly   = np.zeros(ngrid+2*ngc)
     cdef double[:] Lz   = _Lz 
     cdef double[:] L  = np.copy(_Lz)
+    cdef double[:] lx   = np.zeros(ngrid+2*ngc)
+    cdef double[:] ly   = np.zeros(ngrid+2*ngc)
+    cdef double[:] lz   = _Lz/np.copy(_Lz) 
 
-    # Cell interfaces 
+    # Cell interface reconstructions
     cdef double[:] Lx_L      = np.zeros(ngrid+2*ngc)
     cdef double[:] Lx_R      = np.zeros(ngrid+2*ngc)
     cdef double[:] Ly_L      = np.zeros(ngrid+2*ngc)
@@ -216,13 +236,23 @@ def evolve(*p):
     cdef double[:] L_L       = np.zeros(ngrid+2*ngc)
     cdef double[:] L_R       = np.zeros(ngrid+2*ngc)
 
-    # Fluxes; only interior cell faces are used. Here, F_z[i] corresponds to the *left* side. 
-    cdef double[:] F_z       = np.zeros(ngrid+2*ngc)#np.zeros(ngrid+1)
+    # Gradients or gradient-dependent quantities
+    cdef double[:] psi       = np.zeros(ngrid+2*ngc)
+    cdef double[:] Q1        = np.zeros(ngrid+2*ngc)#np.ones(ngrid+2*ngc) * (-1.5*alpha) # this is the taylor expansion for zero warp
+    cdef double[:] Q2        = np.zeros(ngrid+2*ngc)
+    cdef double[:] Q3        = np.zeros(ngrid+2*ngc)
+
+    # Fluxes; only interior cell faces are used. Here, F_[i] corresponds to the *left* side. 
+    cdef double[:] F_x       = np.zeros(ngrid+2*ngc)
+    cdef double[:] F_y       = np.zeros(ngrid+2*ngc)
+    cdef double[:] F_z       = np.zeros(ngrid+2*ngc)
 
     # miscellaneous variables
     cdef double tmp_slope # for data reconstruction
+    cdef double small = 1e-10 # just a real small number! for avoiding 1/0 errors. 
     cdef int i
-    cdef double[:] Lz_inf = np.copy(_Lz[ngrid+2*ngc-2:ngrid+2*ngc])
+    cdef double[:] Lx_old = np.zeros(ngrid+2*ngc)#np.copy(_Lx)
+    cdef double[:] Ly_old = np.zeros(ngrid+2*ngc)#np.copy(_Ly)
     cdef double[:] Lz_old = np.copy(_Lz)
 
     # io variables
@@ -232,23 +262,31 @@ def evolve(*p):
     cdef int       predictor = 1
     cdef double    t = 0.
 
-    # for simple problem
-    cdef double[:] x = _x
-    cdef double[:] Q1 = np.ones(ngrid+2*ngc) * (-1.5*alpha) # this is the taylor expansion for zero warp
-    cdef double[:] Q2 = np.zeros(ngrid+2*ngc)
-    cdef double[:] Q3 = np.zeros(ngrid+2*ngc)
-
-
     # for grid
+    cdef double[:] x = _x
     cdef double dx = _dx
     cdef double dt = 1000000000000000000.
-    cdef double nu = fabs(2.*Q1[0])*HoR**2.
-    cdef double vel = fabs(Q1[0])*HoR**2.
+
+    # for dt and tmax determination. Calculate zero-warp viscosity coefficient to determine "viscous time" of disk. 
+    Q1_tmp = -1.0*10**(interp_1d(s_arr,Q1_arr,0,ng_Q))
+    cdef double nu = fabs(2.*Q1_tmp)*HoR**2.
+    cdef double vel = fabs(Q1_tmp)*HoR**2.
     cdef double t_viscous = (xmax-xmin)**2. / nu
+
+    # determine initial dt from wave velocity and viscosity
     dt  = fabs(cfl*(dx/vel)/(1. + 2*nu/dx/vel))
     tmax *= t_viscous
 
-    ### Do initial outputs
+
+    #################################
+    #################################
+    ########## simulate :) ##########
+    #################################
+    #################################
+
+    ########################
+    ## do initial outputs ##
+    ########################
     printf("Doing initial output...\n")
     sprintf(io_fn,"%s%d.csv",io_prefix,io_cnt)
     f_out = fopen(io_fn,"w")
@@ -265,25 +303,59 @@ def evolve(*p):
     fclose(f_out)
     io_cnt += 1
 
-    # iterate!
+    ######################
+    ## begin iteration! ##
+    ######################
     while (t < tmax):
  
-        ### Reconstruct, Evolve, Average algorithm
-        ## reconstruct
+        #########################
+        ## data reconstruction ##
+        #########################
         for i in range(ngrid+2*ngc):
+
+            ## For Lx/Ly/Lz, calculate the cell slopes based on the desired spatial order of the scheme, and reconstruct the corresponding structure in the cell
+            # Lx
+            if ((space_order==1) or (i==0) or (i==(ngrid+2*ngc-1))): # do piecewise constant reconstruction for outer-most guard cells
+                tmp_slope = 0.0
+            if (space_order==2):
+                tmp_slope = minmod((Lx[i]-Lx[i-1])/dx,(Lx[i+1]-Lx[i])/dx)
+            Lx_L[i] = Lx[i] - 0.5*dx*tmp_slope
+            Lx_R[i] = Lx[i] + 0.5*dx*tmp_slope
+            # Ly
+            if ((space_order==1) or (i==0) or (i==(ngrid+2*ngc-1))): # do piecewise constant reconstruction for outer-most guard cells
+                tmp_slope = 0.0
+            if (space_order==2):
+                tmp_slope = minmod((Ly[i]-Ly[i-1])/dx,(Ly[i+1]-Ly[i])/dx)
+            Ly_L[i] = Ly[i] - 0.5*dx*tmp_slope
+            Ly_R[i] = Ly[i] + 0.5*dx*tmp_slope
+            # Lz
             if ((space_order==1) or (i==0) or (i==(ngrid+2*ngc-1))): # do piecewise constant reconstruction for outer-most guard cells
                 tmp_slope = 0.0
             if (space_order==2):
                 tmp_slope = minmod((Lz[i]-Lz[i-1])/dx,(Lz[i+1]-Lz[i])/dx)
             Lz_L[i] = Lz[i] - 0.5*dx*tmp_slope
             Lz_R[i] = Lz[i] + 0.5*dx*tmp_slope
-
-
+            # Get magnitude of L
             L_L[i]  = (Lx_L[i]**2. + Ly_L[i]**2. + Lz_L[i]**2.)**0.5
             L_R[i]  = (Lx_R[i]**2. + Ly_R[i]**2. + Lz_R[i]**2.)**0.5
 
+            ## For any gradient quantities, evaluate at cell interfaces here. 
+            ## Only need to calculate for the ngrid+1 interfaces of the cell interfaces
+            if not((i==0) or (i==1) or (i==(ngrid+2*ngc-1))):
+                # warp parameter psi
+                psi[i] = ((lx[i]-lx[i-1])**2. + (ly[i]-ly[i-1])**2. + (lz[i]-lz[i-1])**2.)**(0.5) / dx
 
-        ## evolve (get fluxes)
+                # Given warmp amplitude psi[i], Q coefficients must be interpolated from arrays for psi ("s_arr") and Q ("Q1,2,3_arr"), which are of length "ng_Q"
+                Q1[i]      = (-1.0*10**(interp_1d(s_arr,Q1_arr,psi[i],ng_Q)))
+                Q2[i]      = 10**(interp_1d(s_arr,Q2_arr,psi[i],ng_Q))
+                Q3[i]      = 10**(interp_1d(s_arr,Q3_arr,psi[i],ng_Q))
+
+
+
+        ######################
+        ## calculate fluxes ##
+        ######################
+
         for i in range(ngc,ngrid+ngc+1):
             # advective Q1 term
             F_z[i] = (-1.0) * Q1[i]
@@ -300,7 +372,10 @@ def evolve(*p):
             # Apply HoR factor to everything
             F_z[i] *= (HoR)**2.
 
-        ## update
+        #####################
+        ## update solution ##
+        #####################
+
         for i in range(ngc,ngrid+ngc):
             if (time_order==2):
                 if predictor:
@@ -311,13 +386,27 @@ def evolve(*p):
                 Lz[i] = Lz_old[i] - (dt/dx)*(F_z[i+1] - F_z[i])
 
             L[i] = (Lx[i]**2. + Ly[i]**2. + Lz[i]**2.)**0.5
+            lx[i] = Lx[i]/L[i]
+            ly[i] = Ly[i]/L[i]
+            lz[i] = Lz[i]/L[i]
                 
 
+        ###############################
+        ## apply boundary conditions ##
+        ###############################
 
-        ## apply boundary conditions
-        ## Weight by R so sigma is continuous! L should have profile at boundaries
         ## Two guard cells on each end
         if (bc_type==1): #### Apply outflow boundary conditions
+            ## Lx
+            Lx[ngrid+2*ngc-1] = Lx[ngrid-1+ngc]
+            Lx[ngrid+2*ngc-2] = Lx[ngrid-1+ngc]
+            Lx[0] = Lx[2]
+            Lx[1] = Lx[2]
+            ## Ly
+            Ly[ngrid+2*ngc-1] = Ly[ngrid-1+ngc]
+            Ly[ngrid+2*ngc-2] = Ly[ngrid-1+ngc]
+            Ly[0] = Ly[2]
+            Ly[1] = Ly[2]
             ## Lz
             Lz[ngrid+2*ngc-1] = Lz[ngrid-1+ngc]
             Lz[ngrid+2*ngc-2] = Lz[ngrid-1+ngc]
@@ -325,12 +414,42 @@ def evolve(*p):
             Lz[1] = Lz[2]
 
         elif (bc_type==3): #### Infinite disk
-            ### Outer boundary conditions are Dirichlet (set by user):
+            ### Outer boundary conditions are unchanged from initial conditions; inner BCs are outflow.
+            ## Lx
+            Lx[0] = Lx[2]
+            Lx[1] = Lx[2]
+            ## Ly
+            Ly[0] = Ly[2]
+            Ly[1] = Ly[2]
             ## Lz
-            Lz[ngrid+2*ngc-1] = Lz_inf[1]
-            Lz[ngrid+2*ngc-2] = Lz_inf[0]
             Lz[0] = Lz[2]
             Lz[1] = Lz[2]
+
+        ## Also, we want magnitude and unit vectors calculated in our BC, but these are independent of bc_type.
+        # L
+        L[0] = (Lx[0]**2. + Ly[0]**2. + Lz[0]**2.)**0.5
+        L[1] = (Lx[1]**2. + Ly[1]**2. + Lz[1]**2.)**0.5
+        L[ngrid+2*ngc-1] = (Lx[ngrid+2*ngc-1]**2. + Ly[ngrid+2*ngc-1]**2. + Lz[ngrid+2*ngc-1]**2.)**0.5
+        L[ngrid+2*ngc-2] = (Lx[ngrid+2*ngc-2]**2. + Ly[ngrid+2*ngc-2]**2. + Lz[ngrid+2*ngc-2]**2.)**0.5
+        # lx
+        lx[0] = Lx[0]/L[0]
+        lx[1] = Lx[1]/L[1]
+        lx[ngrid+2*ngc-1] = Lx[ngrid+2*ngc-1]/L[ngrid+2*ngc-1]
+        lx[ngrid+2*ngc-2] = Lx[ngrid+2*ngc-2]/L[ngrid+2*ngc-2]
+        # ly
+        ly[0] = Ly[0]/L[0]
+        ly[1] = Ly[1]/L[1]
+        ly[ngrid+2*ngc-1] = Ly[ngrid+2*ngc-1]/L[ngrid+2*ngc-1]
+        ly[ngrid+2*ngc-2] = Ly[ngrid+2*ngc-2]/L[ngrid+2*ngc-2]
+        # lz
+        lz[0] = Lz[0]/L[0]
+        lz[1] = Lz[1]/L[1]
+        lz[ngrid+2*ngc-1] = Lz[ngrid+2*ngc-1]/L[ngrid+2*ngc-1]
+        lz[ngrid+2*ngc-2] = Lz[ngrid+2*ngc-2]/L[ngrid+2*ngc-2]
+
+        ############################
+        ## end of iteration calls ##
+        ############################
             
         if (predictor and (time_order==2)): # predictor step
             predictor = 0 # next while loop iteration due to the full update
@@ -341,6 +460,8 @@ def evolve(*p):
 
             # Update old quantities; these dont change during predictor stage
             for i in range(ngrid+2*ngc):
+                Lx_old[i] = Lx[i]
+                Ly_old[i] = Ly[i]
                 Lz_old[i] = Lz[i]
             predictor = 1 # next while loop iteration do predictor half time step update
 
@@ -363,7 +484,9 @@ def evolve(*p):
                 fclose(f_out)
                 io_cnt += 1 
 
-    ### Do final outputs
+    ######################
+    ## do final outputs ##
+    ######################
     printf("Doing final output...\n")
     sprintf(io_fn,"%s%d.csv",io_prefix,io_cnt)
     f_out = fopen(io_fn,"w")
